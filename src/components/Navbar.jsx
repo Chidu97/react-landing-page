91f33d6ee7546442c73386c4a78efb8da11e95a6
Chidu Ugochukwu <chidu.ugo@hotmail.com>
2023-11-01 09:23:48 +0100

    Minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
   return (
     <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
-      <ul className="hidden md:flex">
+      <ul className="hidden md:flex cursor-pointer">
         <li className="p-4">Home</li>
-        <li className="p-4">Compnay</li>
+        <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
